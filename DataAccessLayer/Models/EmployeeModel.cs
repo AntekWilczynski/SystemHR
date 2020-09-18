e72a34646484adc8df3f94a1f20235a7479aadf1
Antek Wilczyński <a.s.wilczynski@gmail.com>
2020-09-18 20:54:41 +0200

    Added EmployeeModel

---
+++ DataAccessLayer/Models/EmployeeModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemHR.DataAccessLayer.Models
+{
+    public class EmployeeModel
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int Code { get; set; }
+        public GenderModel Gender { get; set; }
+        public DateTime? DataBirth { get; set; }
+        public string PESEL {get;set;}
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string IdentityCardNumber { get; set; }
+        public DateTime? IssueDateIdentytyCard { get; set; }
+
+
+    }
+}
--- SystemHR/Forms/Employees/EmployeesForm.Designer.cs
+++ SystemHR/Forms/Employees/EmployeesForm.Designer.cs
@@ -30,14 +30,16 @@
         {
             this.p = new System.Windows.Forms.TableLayoutPanel();
             this.pEmployees = new System.Windows.Forms.Panel();
-            this.btnCreate = new System.Windows.Forms.Button();
-            this.btnModify = new System.Windows.Forms.Button();
-            this.btnDismiss = new System.Windows.Forms.Button();
-            this.btnRemove = new System.Windows.Forms.Button();
-            this.btnRefresh = new System.Windows.Forms.Button();
             this.btnSendEmail = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnDismiss = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.dgvEmployees = new System.Windows.Forms.DataGridView();
             this.p.SuspendLayout();
             this.pEmployees.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).BeginInit();
             this.SuspendLayout();
             // 
             // p
@@ -45,6 +47,7 @@
             this.p.ColumnCount = 1;
             this.p.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.p.Controls.Add(this.pEmployees, 0, 0);
+            this.p.Controls.Add(this.dgvEmployees, 0, 1);
             this.p.Dock = System.Windows.Forms.DockStyle.Fill;
             this.p.Location = new System.Drawing.Point(0, 0);
             this.p.Name = "p";
@@ -68,61 +71,19 @@
             this.pEmployees.Size = new System.Drawing.Size(794, 34);
             this.pEmployees.TabIndex = 0;
             // 
-            // btnCreate
+            // btnSendEmail
             // 
-            this.btnCreate.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCreate.Image = global::SystemHRUserInterface.Properties.Resources.add_icon;
-            this.btnCreate.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCreate.Location = new System.Drawing.Point(0, 0);
-            this.btnCreate.Name = "btnCreate";
-            this.btnCreate.Size = new System.Drawing.Size(90, 34);
-            this.btnCreate.TabIndex = 0;
-            this.btnCreate.Text = "Dodaj";
-            this.btnCreate.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnCreate.UseVisualStyleBackColor = true;
-            // 
-            // btnModify
-            // 
-            this.btnModify.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnModify.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnModify.Image = global::SystemHRUserInterface.Properties.Resources.edit_icon;
-            this.btnModify.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnModify.Location = new System.Drawing.Point(90, 0);
-            this.btnModify.Name = "btnModify";
-            this.btnModify.Size = new System.Drawing.Size(125, 34);
-            this.btnModify.TabIndex = 1;
-            this.btnModify.Text = "Modyfikuj";
-            this.btnModify.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnModify.UseVisualStyleBackColor = true;
-            // 
-            // btnDismiss
-            // 
-            this.btnDismiss.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnDismiss.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDismiss.Image = global::SystemHRUserInterface.Properties.Resources.delete_icon;
-            this.btnDismiss.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnDismiss.Location = new System.Drawing.Point(215, 0);
-            this.btnDismiss.Name = "btnDismiss";
-            this.btnDismiss.Size = new System.Drawing.Size(90, 34);
-            this.btnDismiss.TabIndex = 2;
-            this.btnDismiss.Text = "Zwolnij";
-            this.btnDismiss.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnDismiss.UseVisualStyleBackColor = true;
-            // 
-            // btnRemove
-            // 
-            this.btnRemove.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRemove.Image = global::SystemHRUserInterface.Properties.Resources.delete_icon;
-            this.btnRemove.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnRemove.Location = new System.Drawing.Point(305, 0);
-            this.btnRemove.Name = "btnRemove";
-            this.btnRemove.Size = new System.Drawing.Size(90, 34);
-            this.btnRemove.TabIndex = 3;
-            this.btnRemove.Text = "Usuń";
-            this.btnRemove.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnSendEmail.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnSendEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSendEmail.Image = global::SystemHRUserInterface.Properties.Resources.internet_icon;
+            this.btnSendEmail.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSendEmail.Location = new System.Drawing.Point(617, 0);
+            this.btnSendEmail.Name = "btnSendEmail";
+            this.btnSendEmail.Size = new System.Drawing.Size(177, 34);
+            this.btnSendEmail.TabIndex = 5;
+            this.btnSendEmail.Text = "Wyślij  e-mail";
+            this.btnSendEmail.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSendEmail.UseVisualStyleBackColor = true;
             // 
             // btnRefresh
             // 
@@ -138,19 +99,73 @@
             this.btnRefresh.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnRefresh.UseVisualStyleBackColor = true;
             // 
-            // btnSendEmail
+            // btnRemove
             // 
-            this.btnSendEmail.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnSendEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSendEmail.Image = global::SystemHRUserInterface.Properties.Resources.internet_icon;
-            this.btnSendEmail.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSendEmail.Location = new System.Drawing.Point(617, 0);
-            this.btnSendEmail.Name = "btnSendEmail";
-            this.btnSendEmail.Size = new System.Drawing.Size(177, 34);
-            this.btnSendEmail.TabIndex = 5;
-            this.btnSendEmail.Text = "Wyślij  e-mail";
-            this.btnSendEmail.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnSendEmail.UseVisualStyleBackColor = true;
+            this.btnRemove.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Image = global::SystemHRUserInterface.Properties.Resources.delete_icon;
+            this.btnRemove.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRemove.Location = new System.Drawing.Point(305, 0);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(90, 34);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Usuń";
+            this.btnRemove.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // btnDismiss
+            // 
+            this.btnDismiss.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnDismiss.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDismiss.Image = global::SystemHRUserInterface.Properties.Resources.delete_icon;
+            this.btnDismiss.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDismiss.Location = new System.Drawing.Point(215, 0);
+            this.btnDismiss.Name = "btnDismiss";
+            this.btnDismiss.Size = new System.Drawing.Size(90, 34);
+            this.btnDismiss.TabIndex = 2;
+            this.btnDismiss.Text = "Zwolnij";
+            this.btnDismiss.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnDismiss.UseVisualStyleBackColor = true;
+            // 
+            // btnModify
+            // 
+            this.btnModify.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnModify.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModify.Image = global::SystemHRUserInterface.Properties.Resources.edit_icon;
+            this.btnModify.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnModify.Location = new System.Drawing.Point(90, 0);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(125, 34);
+            this.btnModify.TabIndex = 1;
+            this.btnModify.Text = "Modyfikuj";
+            this.btnModify.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnModify.UseVisualStyleBackColor = true;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.Image = global::SystemHRUserInterface.Properties.Resources.add_icon;
+            this.btnCreate.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCreate.Location = new System.Drawing.Point(0, 0);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(90, 34);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Dodaj";
+            this.btnCreate.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCreate.UseVisualStyleBackColor = true;
+            // 
+            // dgvEmployees
+            // 
+            this.dgvEmployees.AllowUserToAddRows = false;
+            this.dgvEmployees.AllowUserToDeleteRows = false;
+            this.dgvEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployees.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvEmployees.Location = new System.Drawing.Point(3, 43);
+            this.dgvEmployees.Name = "dgvEmployees";
+            this.dgvEmployees.ReadOnly = true;
+            this.dgvEmployees.Size = new System.Drawing.Size(794, 404);
+            this.dgvEmployees.TabIndex = 1;
             // 
             // EmployeesForm
             // 
@@ -163,6 +178,7 @@
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.EmployeesForm_FormClosed);
             this.p.ResumeLayout(false);
             this.pEmployees.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -177,5 +193,6 @@
         private System.Windows.Forms.Button btnDismiss;
         private System.Windows.Forms.Button btnModify;
         private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.DataGridView dgvEmployees;
     }
 }
