c29cc8ccb392471ae089b15b938d15d48a014572
Antek Wilczyński <a.s.wilczynski@gmail.com>
2020-09-20 19:58:46 +0200

    Added ViewModel

---
+++ DataAccessLayer/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemHR.DataAccessLayer.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+        private string lastName;
+        public string LastName 
+        {
+            get; 
+            set; 
+        }
+        private string firstName;
+        public string FirstName 
+        { 
+            get;
+            set; 
+        }
+        public string Psition { get; set; }
+        public string Status { get; set; }
+    }
+}
